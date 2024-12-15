a5aaa8caff713634fbc9087bab6ff5a0cf6b904e
Yury Kuznetsov <yyk99@kestrel.home>
2024-12-15 14:58:42 -0500

    Update
    - set color to "black"
    - compute output image size

--- dot_generator.py
+++ dot_generator.py
@@ -8,21 +8,28 @@ class dot_generator:
     def __init__(self):
         self._shape = (200,200)
         self._serial = 0
+        self._dpi = 96
         np.random.seed(3)
 
     def generate_one(self):
         x = 4 + np.random.normal(0, 2, 24)
         y = 4 + np.random.normal(0, 2, len(x))
         # size and color:
-        sizes = np.random.uniform(80, 80, len(x))
-        colors = np.random.uniform(250, 250, len(x))
+        sizes = np.repeat(80, len(x))
+        colors = np.repeat(250, len(x))
+
+        # plt.figure(figsize=(800/my_dpi, 800/my_dpi), dpi=my_dpi)
                 
         fig, ax = plt.subplots()
+        fig.set_dpi(self._dpi)
+        fig.set_figwidth(self._shape[0]/self._dpi)
+        fig.set_figheight(self._shape[0]/self._dpi)
+
         # plot
         plt.grid(False)
         plt.axis(False)
         
-        ax.scatter(x, y, s=sizes, c=colors, vmin=0, vmax=100)
+        ax.scatter(x, y, s=sizes, c="black")
         
         ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
             ylim=(0, 8), yticks=np.arange(1, 8))

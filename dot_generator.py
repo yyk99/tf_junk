c7943c909bfa2e33a20468e9f65f62dcdff90bef
Yury Kuznetsov <yyk99@kestrel.home>
2024-12-18 16:55:25 -0500

    Update

--- dot_generator.py
+++ dot_generator.py
@@ -29,7 +29,7 @@ class dot_generator:
         plt.grid(False)
         plt.axis(False)
         
-        ax.scatter(x, y, s=sizes, c="black")
+        ax.scatter(x, y, s=80, c="black")
         
         ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
             ylim=(0, 8), yticks=np.arange(1, 8))
